ad1e7f4469a18bfaa4b8a74a36761b6679d96d6c
Aralo04 <aralotab3@gmail.com>
2023-10-10 08:53:26 +0200

    Red neuronal

--- Exercici1.py
+++ Exercici1.py
@@ -1,10 +1,8 @@
 import numpy as np
 
-
 # Función de activación (por ejemplo, función sigmoide)
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
-
 
 # Clase Neurona
 class Neurona:
@@ -19,16 +17,26 @@ class Neurona:
         salida = sigmoid(suma_ponderada)
         return salida
 
+# Clase Red Neuronal
+class RedNeuronal:
+    def __init__(self, num_neuronas, num_entradas):
+        # Crear un conjunto de neuronas
+        self.neuronas = [Neurona(num_entradas) for _ in range(num_neuronas)]
+
+    def calcular_salida(self, entradas):
+        # Calcular la salida de cada neurona en la capa
+        salidas_capa = [neurona.calcular_salida(entradas) for neurona in self.neuronas]
+        return salidas_capa
 
 # Ejemplo de uso
 if __name__ == "__main__":
-    # Crear una neurona con 3 entradas
-    neurona = Neurona(3)
+    # Crear una red neuronal con 5 neuronas en una capa y 3 entradas
+    red_neuronal = RedNeuronal(5, 2)
 
     # Entradas de ejemplo
-    entradas = np.array([0.5, 0.2, 0.1])
+    entradas = np.array([0.5, 0.2])
 
-    # Calcular la salida de la neurona
-    salida = neurona.calcular_salida(entradas)
+    # Calcular la salida de la red neuronal
+    salida_capa = red_neuronal.calcular_salida(entradas)
 
-    print("Salida de la neurona:", salida)
+    print("Salida de la capa de neuronas:", salida_capa)
